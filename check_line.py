61d43d4952d4da1a962ec58ed3859fdfaf6ebc02
Ana Karla Diaz Rodriguez <anika@almap12.jb.man.ac.uk>
2021-05-06 14:33:53 +0100

    Put spectrum log in temporary file

--- check_line.py
+++ check_line.py
@@ -1,29 +1,33 @@
 #to be run inside CASA
 import numpy as np
+import tempfile
 
 class image_cube:
-	'''This is a image_cube class. It is a CASA image with RA, DEC, and frequency axis.'''
-	
-	def __init__(self, image_name, mask_name, end_name):
-		self.image = 'cube_'+end_name+'.image'
-		self.mask = 'cube_'+end_name+'.mask'
-		importfits(fitsimage=image_name+'.fits',imagename=self.image)
-		importfits(fitsimage=mask_name+'.fits',imagename=self.mask)
+    '''This is a image_cube class. It is a CASA image with RA, DEC, and frequency axis.'''
+    
+    def __init__(self, image_name, mask_name, end_name):
+        self.image = 'cube_'+end_name+'.image'
+        self.mask = 'cube_'+end_name+'.mask'
+        importfits(fitsimage=image_name+'.fits',imagename=self.image)
+        importfits(fitsimage=mask_name+'.fits',imagename=self.mask)
 
-	def make_spectrum(self,log_name):
-		specflux(imagename=self.image, mask=self.mask, unit='MHz',logfile=log_name)
+    def get_spectrum(self,log_name):
+        with tempfile.NamedTemporaryFile(delete=False, suffix='_{}.log'.format(end_name)) as fd:
+            log_file = fd.name
+        specflux(imagename=self.image, mask=self.mask, unit='MHz',logfile=log_file, overwrite=True)
+        return spectrum(log_file)
 
 class spectrum:
-	'''This is a spectrum class. Initialises with a text file.'''
-	
-	def  __init__(self,log_name):
-		ch, n_pix, freq, vel, flux = np.loadtxt(log_name, unpack = True)
-		self.channel = ch
-		self.frequency = freq
-		self.velocity = vel
-		self.flux_density = flux
-	
-	#def make_plot
+    '''This is a spectrum class. Initialises with a text file.'''
+    
+    def  __init__(self,log_name):
+        ch, n_pix, freq, vel, flux = np.loadtxt(log_name, unpack = True)
+        self.channel = ch
+        self.frequency = freq
+        self.velocity = vel
+        self.flux_density = flux
+    
+    #def make_plot
 
 
 image_name = 'member.uid___A001_X133d_X3abb.HOPS-007_sci.spw3.cube.I.manual.image.pbcor'
@@ -34,6 +38,4 @@ log_name='spectrum_'+end_name+'.txt'
 #check that mask image is not just 0s
 
 cube  =  image_cube(image_name, mask_name, end_name)
-cube.make_spectrum(log_name)
-
-spect = spectrum(log_name)
+spect = cube.get_spectrum(log_name)
