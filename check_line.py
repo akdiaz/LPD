7654b4e85951b92c5514232dd0b206efd4b10a15
Ana Karla Diaz Rodriguez <diaz.rodriguez.a.k@gmail.com>
2021-05-13 11:04:22 +0100

    Make several changes in the code:
        - Separate the main instructions in two. One to be run inside CASA 6 (main_step1.py) and the other one to be run in Python (main_step2.py)
        - Separate the modules in two. One to be used inside CASA 6 (get_spectrum_CASA.py) and the other one to be used in Python (check_line.py)
        - Fix potential_lines in Spectrum class
        - Modify make_plot in Spectrum class

--- check_line.py
+++ check_line.py
@@ -1,27 +1,5 @@
-#to be run inside CASA 6.1
 import numpy as np
-import tempfile
 import matplotlib.pyplot as plt
-from casatasks import *
-
-class ImageCube:
-    '''A CASA image with RA, DEC, and frequency axis, with its associated mask.'''
-    
-    def __init__(self, image_name, mask_name, end_name):
-        #check that mask image is not just 0s
-        self.image = 'cube_'+end_name+'.image'
-        self.mask = 'cube_'+end_name+'.mask'
-        importfits(fitsimage=image_name+'.fits',imagename=self.image, overwrite=True)
-        importfits(fitsimage=mask_name+'.fits',imagename=self.mask, overwrite=True)
-        #these images should be deleted later
-        #do I want to keep the spectrum?
-        
-
-    def get_spectrum(self,end_name):
-        with tempfile.NamedTemporaryFile(delete=False, suffix='_{}.txt'.format(end_name)) as fd:
-            log_file = fd.name
-        specflux(imagename=self.image, mask=self.mask, unit='MHz',logfile=log_file, overwrite=True)
-        return Spectrum(log_file)
 
 class Spectrum:
     '''This is a spectrum class. Initialises with a text file.'''
@@ -29,14 +7,8 @@ class Spectrum:
     def  __init__(self,log_file):
         self.frequency, self.flux = np.loadtxt(log_file, usecols=(2,4), unpack = True)
     
-    #this doesn't work with my instalation of CASA 6.1
-    def make_plot(self):
-        fig, ax = plt.subplots()
-        ax.plot(self.frequency, self.flux)
-        fig.savefig(log_file[:-4]+'.png',bbox_inches='tight')
-
     def potential_lines(self, list_file):
-        transition_frequencies = np.loadtxt(list_file, usecols=1)
+        transition_frequencies = np.loadtxt(list_file, usecols=[1])
         species, transitions = np.loadtxt(list_file, usecols=(0,2),dtype='str', unpack=True)
         f_min = min(self.frequency)
         f_max = max(self.frequency)
@@ -46,4 +18,11 @@ class Spectrum:
                 potential_lines.append([species[index], transitions[index], f])
         return potential_lines
 
-
+    def make_plot(self,log_file,lines):
+        fig, ax = plt.subplots()
+        ax.plot(self.frequency, self.flux)
+        y_lims = ax.get_ylim()
+        for l in lines:
+          ax.vlines(l[2], y_lims[0], y_lims[1] ,'r')
+          ax.annotate(' '.join([l[0],l[1]]),(l[2],0.9),xycoords=('data','axes fraction'))
+        fig.savefig(log_file[:-4]+'.png',bbox_inches='tight')
---
+++ main_step2.py
@@ -0,0 +1,7 @@
+import check_line
+
+spectrum_name='spectrum_golden.txt'
+list_file='lines.txt'
+spect = check_line.Spectrum(spectrum_name)
+lines = spect.potential_lines(list_file)
+spect.make_plot(spectrum_name, lines)
