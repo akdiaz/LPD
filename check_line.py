1762464253632a26990947a02fb163b1ee281252
Ana Karla Diaz Rodriguez <diaz.rodriguez.a.k@gmail.com>
2021-06-16 15:31:20 +0100

    Include original (input) column names and units in the output file header

--- check_line.py
+++ check_line.py
@@ -24,34 +24,16 @@ def match_lines(potential_lines, detected_lines_frequency):
         else:
             actual_lines.append(['U','U','0'])
     return actual_lines
-        
-def write_parameters(actual_lines, peak_frequency, peak_velocity, peak_flux):
-        print('Writing output file...')
-        #need to add the unit, make the convertion from sigma to HPBW
-        header = 'Species\tTransition\tFrequency\tPeak_Frequency\tPeak_Velocity\tPeak_Flux\tFlag'
-        molecules = np.array([i[0] for i in actual_lines]) 
-        transitions = np.array([i[1] for i in actual_lines])
-        frequencies = np.array([i[2] for i in actual_lines])
-        #format of columns in data
-        columns_dtype = [('molecule', 'U25'), ('transition', 'U25'), ('frequency', float), ('peak_frequency', float), ('peak_velocity', float), ('peak_flux', float)]
-        fmt = ['%s']*2+['%f']*4
-        #write data
-        data = np.zeros(molecules.size, dtype=columns_dtype)
-        data['molecule'] = molecules
-        data['transition'] = transitions
-        data['frequency'] = frequencies
-        data['peak_frequency'] = peak_frequency
-        data['peak_velocity'] = peak_velocity
-        data['peak_flux'] = peak_flux
-        np.savetxt('detected_lines.txt', data, header=header, fmt=fmt)
-
+    
+      
 class Spectrum:
     '''This is a spectrum class. Initialises with a text file.'''
     
     def  __init__(self,log_file):
+        self.columns = np.loadtxt(log_file, skiprows=3, max_rows=1, comments=None, dtype='str', usecols=(3,4,5))
         self.frequency, self.velocity, self.flux = np.loadtxt(log_file, usecols=(2,3,4), unpack = True)
         self.frequency_resolution = self.frequency[1]-self.frequency[0]
-        self.velocity_resolution = self.velocity[1]-self.velocity[0]
+        self.velocity_resolution = self.velocity[1]-self.velocity[0]        
     
     def potential_lines(self, list_file):
         print('Finding expected lines in the spectrum...')
@@ -75,12 +57,31 @@ class Spectrum:
         popt, _ = curve_fit(gaussian, edges[:-1], values, p0 = [max(self.flux), 0, 1])
         return abs(popt[-1])
 
-    def find_lines(self,separation=WIDTH_LINE):
+    def find_lines(self):
         print('Finding detected lines in the spectrum...')
         #separation is in channels, mind this when changing width_line to velocity
-        peaks = sig.find_peaks(self.flux, height=SNR*self.rms,distance=separation)
+        peaks = sig.find_peaks(self.flux, height=SNR*self.rms,distance=WIDTH_LINE)
         position_peaks = peaks[0]
         return self.frequency[position_peaks], self.velocity[position_peaks], self.flux[position_peaks]
+        
+    def write_parameters(self, actual_lines, peak_frequency, peak_velocity, peak_flux):
+        print('Writing output file...')
+        header = 'Species\tTransition\tFrequency\t'+f'Peak_{self.columns[0]}\tPeak_{self.columns[1]}\tPeak_{self.columns[2]}'
+        molecules = np.array([i[0] for i in actual_lines]) 
+        transitions = np.array([i[1] for i in actual_lines])
+        frequencies = np.array([i[2] for i in actual_lines])
+        #format of columns in data
+        columns_dtype = [('molecule', 'U25'), ('transition', 'U25'), ('frequency', float), ('peak_frequency', float), ('peak_velocity', float), ('peak_flux', float)]
+        fmt = ['%s']*2+['%f']*4
+        #write data
+        data = np.zeros(molecules.size, dtype=columns_dtype)
+        data['molecule'] = molecules
+        data['transition'] = transitions
+        data['frequency'] = frequencies
+        data['peak_frequency'] = peak_frequency
+        data['peak_velocity'] = peak_velocity
+        data['peak_flux'] = peak_flux
+        np.savetxt('detected_lines.txt', data, header=header, fmt=fmt)
        
     #this is just for reference while I code        
     def make_plot(self, log_file, lines, frequency_peaks, flux_peaks):
@@ -104,5 +105,3 @@ class Spectrum:
                 ax.annotate(' '.join([l[0],l[1]]),(l[2],0.9),xycoords=('data','axes fraction'))
         ax.legend()
         fig.savefig(log_file[:-4]+'.png',bbox_inches='tight')
-        
-        
