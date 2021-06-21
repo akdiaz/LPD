c3c45b7ae467939557ebc5adb072486134ec4f32
Ana Karla Diaz Rodriguez <diaz.rodriguez.a.k@gmail.com>
2021-06-21 11:10:11 +0100

    Add default value for spectrum_file_name

--- liner.py
+++ liner.py
@@ -5,7 +5,7 @@ parser = argparse.ArgumentParser(description='LineR: Line Recognizer. Searches f
 parser.add_argument('-t','--frequency_tolerance', type=float, help='Frequency tolerance (in the frequency units of the input spectrum). If a detected line has a known line with a frequency separation less than <<frequency_tolerance>>, it will assume its ID, if not, will remain unidentified (U).', default = 2)
 parser.add_argument('-w','--line_width', type=float, help='Line width (in channels). Local peaks closer than <<line_width>> will be considered as belonging to the same line, and only the one with higher flux will be returned.', default = 20)
 parser.add_argument('--snr', type=float, help='Signal-to-noise ratio. Only peaks with flux higher than <<SNR>> will be returned', default = 5)
-parser.add_argument('-s','--spectrum_file_name', type=str, help='Name of the file (including extension) with the spectrum to analyse.')
+parser.add_argument('-s','--spectrum_file_name', type=str, help='Name of the file (including extension) with the spectrum to analyse.', default = 'spectrum_golden.txt')
 parser.add_argument('-l','--known_lines_file_name', type=str, help='Name of the file (including extension) with the known lines.', default='lines.txt')
 
 if __name__ == "__main__": 
