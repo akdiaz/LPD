f92c1e7c9f441bfc2c7297ce480ec1794743bb1e
Ana Karla Diaz Rodriguez <diaz.rodriguez.a.k@gmail.com>
2021-06-21 10:50:35 +0100

    Change module name to get_spectrum.py

--- main_step1.py
+++ main_step1.py
@@ -1,7 +1,7 @@
 #to be run inside CASA 6.1
 import sys, os
 sys.path.append(os.getcwd()) #to be able to find check_line
-import get_spectrum_CASA
+import get_spectrum
 
 image_name = 'golden.image.pbcor'
 mask_name = 'golden.mask'
@@ -9,4 +9,3 @@ end_name = 'golden'
 
 cube  = get_spectrum_CASA.ImageCube(image_name, mask_name, end_name)
 cube.get_spectrum(end_name)
-
