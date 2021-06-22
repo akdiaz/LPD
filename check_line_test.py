0fdde343cea45c59a2dbab4e7aef331a99990ac6
Ana Karla Diaz Rodriguez <diaz.rodriguez.a.k@gmail.com>
2021-06-22 17:07:48 +0100

    Format with black

--- check_line_test.py
+++ check_line_test.py
@@ -1,28 +1,32 @@
 import unittest
 import sys, os
-sys.path.append(os.getcwd()) #to be able to find check_line
+
+sys.path.append(os.getcwd())  # to be able to find check_line
 import check_line
 import numpy as np
 
-TEST_DATA_PATH = 'tests/data'
+TEST_DATA_PATH = "tests/data"
 
-IMAGE_NAME = 'golden.image.pbcor'
-MASK_NAME = 'golden.mask'
-END_NAME = 'test'
-GOLDEN_SPECTRUM = 'spectrum_golden.txt'
+IMAGE_NAME = "golden.image.pbcor"
+MASK_NAME = "golden.mask"
+END_NAME = "test"
+GOLDEN_SPECTRUM = "spectrum_golden.txt"
 
 image_path = os.path.join(TEST_DATA_PATH, IMAGE_NAME)
 mask_path = os.path.join(TEST_DATA_PATH, MASK_NAME)
 golden_spectrum_path = os.path.join(TEST_DATA_PATH, GOLDEN_SPECTRUM)
 
+
 class SpectrumTest(unittest.TestCase):
-    
     def test_take_spectrum(self):
-        ''' Make sure that the spectrum is taken correctly.'''
-        cube  = check_line.ImageCube(image_path, mask_path, END_NAME)
+        """Make sure that the spectrum is taken correctly."""
+        cube = check_line.ImageCube(image_path, mask_path, END_NAME)
         spectrum = cube.get_spectrum(END_NAME)
-        frequency_golden, flux_golden = np.loadtxt(golden_spectrum_path, usecols=(2,4), unpack = True)
+        frequency_golden, flux_golden = np.loadtxt(
+            golden_spectrum_path, usecols=(2, 4), unpack=True
+        )
         self.assertListEqual(list(spectrum.frequency), list(frequency_golden))
         self.assertListEqual(list(spectrum.flux), list(flux_golden))
+
 
 unittest.main(exit=False, verbosity=2)
--- get_spectrum_CASA.py
+++ get_spectrum_CASA.py
@@ -1,11 +1,12 @@
-#to be run inside CASA 6.1
+# to be run inside CASA 6.1
 import sys, os
-sys.path.append(os.getcwd()) #to be able to find check_line
+
+sys.path.append(os.getcwd())  # to be able to find check_line
 import get_spectrum
 
-image_name = 'golden.image.pbcor'
-mask_name = 'golden.mask'
-end_name = 'golden'
+image_name = "golden.image.pbcor"
+mask_name = "golden.mask"
+end_name = "golden"
 
-cube  = get_spectrum_CASA.ImageCube(image_name, mask_name, end_name)
+cube = get_spectrum_CASA.ImageCube(image_name, mask_name, end_name)
 cube.get_spectrum(end_name)
