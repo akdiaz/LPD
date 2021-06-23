bf6785ad186e9c74939ed8e117cbb6d3365bbef3
Ana Karla Diaz Rodriguez <diaz.rodriguez.a.k@gmail.com>
2021-06-23 07:22:58 +0100

    Add radial velocity of the source as an input option. Fix #14
        * Redshift the teoretical frequencies of the known lines before making the match with the detected lines
        * Write as output also the redshifted frequencies
        * Mark in the plot the redshifted frequencies instead of the teorical

--- check_line.py
+++ check_line.py
@@ -4,27 +4,34 @@ import scipy.signal as sig
 from scipy.optimize import curve_fit
 from sys import exit
 from functools import lru_cache
+import astropy.units as u
 
 
 def gaussian(x, a, x0, sigma):
     y = a * np.exp(-((x - x0) ** 2) / (2 * sigma ** 2))
     return y
 
+def redshift_frequency(f, vlsr):
+    """Assumes (for now) rest frequency (f) in MHz and velocity of the source (vlsr) in km/s."""
+    f_rest = f * u.MHz  # rest frequency
+    vlsr = vlsr * u.km / u.s  # velocity of the source
+    relativistic_equiv = u.doppler_relativistic(f_rest)
+    f_shifted = vlsr.to(u.MHz, equivalencies=relativistic_equiv)
+    return f_shifted.value
 
 def match_lines(potential_lines, detected_lines_frequency, tolerance):
-    potential_frequency = np.array([pot_line[2] for pot_line in potential_lines])
+    potential_frequency = np.array([pot_line[3] for pot_line in potential_lines])
     actual_lines = []
     for index, frequency in enumerate(detected_lines_frequency):
         distances = potential_frequency - frequency
         if min(abs(distances)) > tolerance:
-            actual_lines.append([index, "U", "U", "0"])
+            actual_lines.append([index, "U", "U", "0", "0"])
         else:
             for index_dist, dist in enumerate(distances):
                 if dist < tolerance:
                     actual_lines.append([index] + potential_lines[index_dist])
     return actual_lines
-
-
+    
 class Spectrum:
     """This is a spectrum class. Initialises with a text file."""
 
@@ -43,17 +50,19 @@ class Spectrum:
         self.frequency_resolution = self.frequency[1] - self.frequency[0]
         self.velocity_resolution = self.velocity[1] - self.velocity[0]
 
-    def potential_lines(self, list_file):
+    def potential_lines(self, list_file, vlsr):
         print("Finding expected lines in the spectrum...")
         transition_frequencies = np.loadtxt(list_file, usecols=[1])
         species, transitions = np.loadtxt(
             list_file, usecols=(0, 2), dtype="str", unpack=True
         )
         minimum, *_, maximum = sorted(self.frequency)
+        print("\t Redshifting teorical frequencies...")
+        redshifted_frequencies = redshift_frequency(transition_frequencies, vlsr)
         potential_lines = []
-        for index, frequency in enumerate(transition_frequencies):
+        for index, frequency in enumerate(redshifted_frequencies):
             if minimum <= frequency <= maximum:
-                potential_lines.append([species[index], transitions[index], frequency])
+                potential_lines.append([species[index], transitions[index], transition_frequencies[index], frequency])
         if len(potential_lines) == 0:
             exit(
                 "There is no potential lines in the frequency range of the spectrum. Edit the file <<lines.txt>>."
@@ -82,13 +91,14 @@ class Spectrum:
     def write_parameters(self, actual_lines, peak_frequency, peak_velocity, peak_flux):
         print("Writing output file...")
         header = (
-            "Peak\tSpecies\tTransition\tFrequency\t"
+            "Peak\tSpecies\tTransition\tTeorical_Frequency\tRedshifted_Frequency\t"
             + f"Peak_{self.columns[0]}\tPeak_{self.columns[1]}\tPeak_{self.columns[2]}"
         )
         peaks = np.array([i[0] for i in actual_lines])
         molecules = np.array([i[1] for i in actual_lines])
         transitions = np.array([i[2] for i in actual_lines])
         frequencies = np.array([i[3] for i in actual_lines])
+        redshifted_frequencies = np.array([i[4] for i in actual_lines])
         peak_frequencies = [peak_frequency[p] for p in peaks]
         peak_velocities = [peak_velocity[p] for p in peaks]
         peak_fluxes = [peak_flux[p] for p in peaks]
@@ -98,17 +108,19 @@ class Spectrum:
             ("molecule", "U25"),
             ("transition", "U25"),
             ("frequency", float),
+            ("redshifted_frequency", float),
             ("peak_frequency", float),
             ("peak_velocity", float),
             ("peak_flux", float),
         ]
-        fmt = ["%i"] + ["%s"] * 2 + ["%f"] * 4
+        fmt = ["%i"] + ["%s"] * 2 + ["%f"] * 5
         # write data
         data = np.zeros(molecules.size, dtype=columns_dtype)
         data["peak"] = peaks
         data["molecule"] = molecules
         data["transition"] = transitions
         data["frequency"] = frequencies
+        data["redshifted_frequency"] = redshifted_frequencies
         data["peak_frequency"] = peak_frequencies
         data["peak_velocity"] = peak_velocities
         data["peak_flux"] = peak_fluxes
@@ -144,7 +156,7 @@ class Spectrum:
                     l[1], (frequency_peaks[l[0]], flux_peaks[l[0]]), xycoords=("data")
                 )
             else:
-                ax.vlines(l[3], y_lims[0], y_lims[1], "b")
+                ax.vlines(l[4], y_lims[0], y_lims[1], "b")
                 ax.annotate(
                     " ".join([l[1], l[2]]),
                     (l[3], 0.9),
