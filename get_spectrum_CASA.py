b1e96605e28c248980bd854e9dcf0bd3f79e014b
Ana Karla Diaz Rodriguez <diaz.rodriguez.a.k@gmail.com>
2021-06-28 20:46:47 +0100

    Process all the files ending in '.image.pbcor.fits' in the current folder and teheir respective masks ending in '.mask.fits'. Fix #6

--- get_spectrum_CASA.py
+++ get_spectrum_CASA.py
@@ -1,12 +1,20 @@
 # to be run inside CASA 6.1
 import sys, os
+import argparse
+import glob
+
 
 sys.path.append(os.getcwd())  # to be able to find check_line
 import get_spectrum
 
-image_name = "golden.image.pbcor"
-mask_name = "golden.mask"
-end_name = "golden"
+if __name__ == "__main__":
 
-cube = get_spectrum_CASA.ImageCube(image_name, mask_name, end_name)
-cube.get_spectrum(end_name)
+    image_name = glob.glob('*.image.pbcor.fits')
+    
+    for cube_name in image_name:
+        base_name = cube_name[:-17]
+        mask_name = base_name+".mask.fits"
+        spectrum_name = base_name+".spectrum.txt"
+        print(f"For {base_name}...")
+        cube = get_spectrum.ImageCube(base_name)
+        cube.get_spectrum()
