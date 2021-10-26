bac631f6d5d12d0b1dcaa571a5c3442812b55d0d
Ana Karla Diaz Rodriguez <diaz.rodriguez.a.k@gmail.com>
2021-10-26 17:31:56 +0100

    Add test for match_lines

--- check_line_test.py
+++ check_line_test.py
@@ -18,7 +18,14 @@ class LinesTest(unittest.TestCase):
         self.assertEqual(redshifted_frequency, 345794.8364506588)
 
 
-
+    def test_match_lines(self):
+        """Make sure that the lines are correctly matched"""
+        potential_lines = [['13COv=0', 'J=3-2,F=5/2-5/2', 330587.8671, 330587.8671], ['13COv=0', 'J=3-2,F=7/2-5/2', 330587.9816, 330587.98160]]
+        detected_lines_frequency = [330587.8671, 330588.8671]
+        tolerance = 0.01
+        actual_lines = check_line.match_lines(potential_lines, detected_lines_frequency, tolerance)
+        expected_output = [[0, '13COv=0', 'J=3-2,F=5/2-5/2', 330587.8671, 330587.8671], [1, "U", "U", "0", "0"]]
+        self.assertEqual(actual_lines, expected_output)
 
 if __name__ == "__main__":
     unittest.main( verbosity=2)
