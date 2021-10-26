331f428b75c2d3485d9f8416118789116f2bdeb9
Ana Karla Diaz Rodriguez <diaz.rodriguez.a.k@gmail.com>
2021-10-26 16:51:19 +0100

    Add test for redshift_frequency

--- check_line_test.py
+++ check_line_test.py
@@ -6,4 +6,19 @@ import sys, os
 
 sys.path.append(os.getcwd())  # to be able to find check_line
 import check_line
-import numpy as np
+
+
+
+class LinesTest(unittest.TestCase):
+    def test_redshifted(self):
+        """Make sure that the frequency is correctly refshifted"""
+        frequency = 345795.98990 #MHz
+        vlsr = 1.0  #km/s
+        redshifted_frequency = check_line.redshift_frequency(frequency, vlsr)
+        self.assertEqual(redshifted_frequency, 345794.8364506588)
+
+
+
+
+if __name__ == "__main__":
+    unittest.main( verbosity=2)
