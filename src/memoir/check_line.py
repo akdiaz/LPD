04272c70c5342d89fc804c9762b22322517bafb6
Ana Karla Diaz Rodriguez <diaz.rodriguez.a.k@gmail.com>
2022-09-16 15:41:57 +0200

    Fix typo

--- src/memoir/check_line.py
+++ src/memoir/check_line.py
@@ -277,7 +277,7 @@ class Spectrum:
         self, peak_velocity, peak_flux, peak_width, log_file
     ):
         print("Writing output file...")
-        output_file = "estimates.txt"
+        output_file = "estimate.txt"
         header = (
             "Spectrum_Peak_ID\t"
             + f"Peak_{self.columns[1]}\tPeak_{self.columns[2]}\tPeak_Width_FWHM {self.columns[1].split('_')[-1]}"
